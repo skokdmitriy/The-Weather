c38b1bc8b0653ed571b5558deff20a3773722b09
Dmitriy <skokdmitriy@gmail.com>
2023-04-14 22:49:04 +0700

    Project Setup

---
+++ The Weather/Application/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  The Weather
+//
+//  Created by Дмитрий Скок on 14.04.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.windowScene = windowScene
+        let rootViewController = ViewController()
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
+    }
+}
+
